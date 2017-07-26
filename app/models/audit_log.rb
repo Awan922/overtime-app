c7121c9eaeeaf351072ed3e435b13caeb82c9bad
Muhammad Awan <msa922@yahoo.com>
2017-07-26 10:55:42 -0600

    Refactored button functionality for employee dashboard

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,5 +1,5 @@
 class AuditLog < ActiveRecord::Base
-	enum status: { pending: 0, confirmed: 1 }
+  enum status: { pending: 0, confirmed: 1 }
 
   belongs_to :user
 
@@ -7,11 +7,13 @@ class AuditLog < ActiveRecord::Base
 
   after_initialize :set_defaults
 
- before_update :set_end_date, if: :confirmed?  
+  before_update :set_end_date, if: :confirmed?
+
+  scope :by_start_date, -> { order('start_date DESC') }
 
   private
     def set_end_date
-     self.end_date = Date.today
+      self.end_date = Date.today
     end
 
     def set_defaults
